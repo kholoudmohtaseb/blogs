9227c9f69e171e72cde271f0b8adf400dd72177a
kholoudmohtaseb <kholoud.almohtseb@gmail.com>
2021-04-03 03:48:07 +0300

    blogs

---
+++ blogs/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Blogs</h1>
+            <div className="link">
+                <a href="/">Home</a>
+                <a href="/create">New blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
